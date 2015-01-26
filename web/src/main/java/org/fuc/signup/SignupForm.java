9d18446de85bb6c51bc5730651e255abb164b1ce
Valentin Korolyov <kor.val@inbox.ru>
2015-01-26 11:20:59 +0300

    Username must not be email any more.

--- web/src/main/java/org/fuc/signup/SignupForm.java
+++ web/src/main/java/org/fuc/signup/SignupForm.java
@@ -1,18 +1,15 @@
 package org.fuc.signup;
 
-import org.fuc.config.RoleProvider;
+import org.fuc.entities.RoleProvider;
 import org.fuc.entities.Account;
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
 
 public class SignupForm {
 
 	private static final String NOT_BLANK_MESSAGE = "{notBlank.message}";
-	private static final String EMAIL_MESSAGE = "{email.message}";
 
-    @NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
-	@Email(message = SignupForm.EMAIL_MESSAGE)
+	@NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
 	private String email;
 
     @NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
